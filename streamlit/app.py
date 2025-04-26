87869d13f338837aa7cab1a388e715e1e1e24617
Vikas Mishra <vikasmishra.webdev@gmail.com>
2025-04-26 12:19:20 +0000

    streamlit

---
+++ streamlit/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+## title of the application
+st.title("Hello Streamlit")
+
+## Display simple text
+st.write("This is a simple text")
